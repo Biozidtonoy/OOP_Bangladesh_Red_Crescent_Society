f4cce08c3dd5df8cbe79f279d17ed4536001755f
Biozid Bhuiuan Tonoy <tawsiftonoy007@gmail.com>
2024-03-24 03:52:50 +0600

    can register account and can login eith register account

--- Oop_Bangladesh_Red_Crescent_Society/OOP_Bangladesh_Red_Crescent_Society/src/startPage/LoginSceneController.java
+++ Oop_Bangladesh_Red_Crescent_Society/OOP_Bangladesh_Red_Crescent_Society/src/startPage/LoginSceneController.java
@@ -6,12 +6,21 @@ package startPage;
 
 import helperClass.Utility;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import static startPage.RegisterSceneController.username;
+import static startPage.RegisterSceneController.userpass;
 
 /**
  * FXML Controller class
@@ -24,18 +33,42 @@ public class LoginSceneController implements Initializable {
     private TextField usernameTF;
     @FXML
     private PasswordField passwordTF;
+    
+    Alert invaldInput = new Alert(Alert.AlertType.INFORMATION,"invalid username and password");
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
 
     @FXML
-    private void loginButton(ActionEvent event) throws Exception {
-//        Utility.sceneSwitch (getClass(),event,"/Tonoy/Director/directorDashboard.fxml");
+    private void loginButton(ActionEvent event) throws Exception  {
+        String enteredUserName = usernameTF.getText();
+        String enteredPassword = passwordTF.getText();
+
+        ArrayList<String> username = RegisterSceneController.username;
+        ArrayList<String> userpass = RegisterSceneController.userpass;
+        
+        boolean validLogin = false;
+
+        for (int i = 0; i < username.size(); i++) {
+            if (username.get(i).equals(enteredUserName) && userpass.get(i).equals(enteredPassword)) {
+                validLogin = true;
+                break;
+            }
+        }
+
+        if (validLogin) {
+//        Scene Switching
+          Utility.sceneSwitch (getClass(),event,"/Tonoy/Director/directorDashboard.fxml");
+            
+        } else {
+            invaldInput.show();
+        }
+
     }
 
     @FXML
--- Oop_Bangladesh_Red_Crescent_Society/OOP_Bangladesh_Red_Crescent_Society/src/startPage/RegisterSceneController.java
+++ Oop_Bangladesh_Red_Crescent_Society/OOP_Bangladesh_Red_Crescent_Society/src/startPage/RegisterSceneController.java
@@ -4,12 +4,21 @@
  */
 package startPage;
 
+import helperClass.User;
 import helperClass.Utility;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * FXML Controller class
@@ -18,17 +27,99 @@ import javafx.fxml.Initializable;
  */
 public class RegisterSceneController implements Initializable {
 
+    @FXML
+    private TextField fullnameTF;
+    @FXML
+    private TextField usernameTF;
+    @FXML
+    private PasswordField passwordTF;
+    @FXML
+    private TextField emailTF;
+    @FXML
+    private DatePicker birthdayTF;
+    @FXML
+    private RadioButton maleRadio;
+    @FXML
+    private RadioButton femaleRadio;
+    @FXML
+    private RadioButton otherRadio;
+    
+    ToggleGroup tg =new ToggleGroup();
+    Alert unfilled = new Alert(Alert.AlertType.WARNING,"Please Enter Everything!");
+    Alert invalid = new Alert(Alert.AlertType.WARNING,"invalid username or password !");
+    Alert success = new Alert(Alert.AlertType.INFORMATION,"Congratulation your account has been created ");
+    
+    /**
+     *
+     */
+    public static ArrayList<User> userDetails = new ArrayList<>();
+    public static ArrayList<String> username = new ArrayList<>();
+    public static ArrayList<String> userpass = new ArrayList<>();
+   
+    
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        maleRadio.setToggleGroup(tg);
+        femaleRadio.setToggleGroup(tg);
+        otherRadio.setToggleGroup(tg);
+        
+        
     }    
 
+    @FXML
+    private void signupButton(ActionEvent event) {
+        String fullName = fullnameTF.getText();
+        String userName = usernameTF.getText();
+        String email = emailTF.getText();
+        String userPass = passwordTF.getText();
+        LocalDate birthday = birthdayTF.getValue();
+        String gender = "";
+        
+        User u = new User(fullName,userName,email,userPass,birthday,gender);
+        userDetails.add(u);
+
+        username.add(userName);
+        userpass.add(userPass);
+
+
+      
+        if(maleRadio.isSelected())
+            gender = "male";
+        else if( femaleRadio.isSelected())
+            gender = "female";
+        else if (otherRadio.isSelected())
+            gender = " other";
+        
+        if (gender.isEmpty()){
+            unfilled.show();
+            return;
+        }
+        
+        boolean anyEmpty = false;
+            if (fullName.isEmpty()||userName.isEmpty()||email.isEmpty()||userPass.isEmpty()||birthday==null){
+                anyEmpty = true;
+               
+            }
+        
+        if (anyEmpty){
+            unfilled.show();
+        }
+        
+        success.show();
+        
+       
+        
+
+
+      
+    }
     @FXML
     private void backButton(ActionEvent event) throws Exception {
         Utility.sceneSwitch (getClass(),event,"/startPage/loginScene.fxml");
     }
-    
+
 }
