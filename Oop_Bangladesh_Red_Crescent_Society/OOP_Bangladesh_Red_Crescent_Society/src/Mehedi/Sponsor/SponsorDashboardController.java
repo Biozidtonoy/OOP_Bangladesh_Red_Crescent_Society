d42b3ca7d2c24ceebdf73790aca882574723946e
Mehedii-07 <mehedihasan2272002@gmail.com>
2024-03-31 03:03:32 +0600

    deshboardcontrollermaking

---
+++ Oop_Bangladesh_Red_Crescent_Society/OOP_Bangladesh_Red_Crescent_Society/src/Mehedi/Sponsor/SponsorDashboardController.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Mehedi.Sponsor;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Elitebook 830
+ */
+public class SponsorDashboardController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void bookingBTclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void paymentBTclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void longtermsponsorshipBTclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void attendeventBTclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void feedbackBTclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void collaborateBTclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void monitorprojectBTclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void logoutBTclick(ActionEvent event) throws IOException {
+        Parent mainSceneParent = FXMLLoader.load(getClass().getResource("/mainpkg/startScene.fxml"));
+        Scene scene1 = new Scene(mainSceneParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow(); 
+        window.setScene(scene1);
+        window.show();
+    }
+    
+}
