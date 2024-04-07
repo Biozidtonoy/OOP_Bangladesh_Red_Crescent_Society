5b40fc8df33a60bc495e840ae78757a5f85f5013
Biozid Bhuiuan Tonoy <tawsiftonoy007@gmail.com>
2024-04-07 23:34:40 +0600

    survey fxml and survey .BIn file created

--- Oop_Bangladesh_Red_Crescent_Society/OOP_Bangladesh_Red_Crescent_Society/src/Tonoy/Donor/SurveyController.java
+++ Oop_Bangladesh_Red_Crescent_Society/OOP_Bangladesh_Red_Crescent_Society/src/Tonoy/Donor/SurveyController.java
@@ -4,9 +4,13 @@
  */
 package Tonoy.Donor;
 
+import helperClass.Survey;
+import helperClass.feedback;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,6 +18,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -23,16 +31,92 @@ import javafx.stage.Stage;
  */
 public class SurveyController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
+    @FXML
+    private TextField emailTF;
+    @FXML
+    private TextField q1TF;
+    @FXML
+    private TextField occupationTF;
+    @FXML
+    private ComboBox<String> genderCB;
+    @FXML
+    private ComboBox<String> ageCB;
+    @FXML
+    private TextField q2TF;
+    @FXML
+    private TextField q4TF;
+    @FXML
+    private TextField q5TF;
+    @FXML
+    private TextField q6TF;
+    @FXML
+    private TextField q7TF;
+    @FXML
+    private TextField q8TF;
+    @FXML
+    private TextField q9TF;
+    @FXML
+    private TextField q3TF;
+    @FXML
+    private ComboBox<String> selectYesorNoCb;
+   
+
+     public static ArrayList<Survey> surveyList = new ArrayList<Survey>();
+    Alert success = new Alert(Alert.AlertType.INFORMATION,"Survey done");
+    Alert unfilled = new Alert(Alert.AlertType.WARNING,"Please Enter Everything!");
+    Alert cancel = new Alert(Alert.AlertType.WARNING,"your donation has cancelled");
+    @FXML
+    
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        ageCB.setItems(FXCollections.observableArrayList("age >= 18 && age <= 25","age > 25 && age <= 30"," age > 30 && age <= 35","age>35"));
+        genderCB.setItems(FXCollections.observableArrayList("Male","female","other"));
+        selectYesorNoCb.setItems(FXCollections.observableArrayList("Yes","No"));
+        
     }    
 
     @FXML
     private void submitBT(ActionEvent event) {
+     try {
+                 
+            String email = emailTF.getText().trim();
+            String age = ageCB.getValue();
+            String gender = genderCB.getValue();
+            String occupation = occupationTF.getText().trim();
+            String q1 = q1TF.getText().trim();
+            String q2 = q2TF.getText().trim();
+            String q3 = q3TF.getText().trim();
+            String q4 = q4TF.getText().trim();
+            String q5 = q5TF.getText().trim();
+            String q6 = q6TF.getText().trim();
+            String q7 = q7TF.getText().trim();
+            String q8 = q8TF.getText().trim();
+            String q9 = q9TF.getText().trim();
+            String q10 =selectYesorNoCb.getValue();;
+            
+            
+          
+           
+
+            if (email.isEmpty()||ageCB==null||genderCB==null||occupation.isEmpty()||q1.isEmpty()||
+                    q2.isEmpty()||q3.isEmpty()||q4.isEmpty()||q5.isEmpty()||q6.isEmpty()||
+                    q7.isEmpty()||q8.isEmpty()||q9.isEmpty()||q10.isEmpty()||selectYesorNoCb==null) {
+                unfilled.show();
+                return;
+
+            }
+            Survey s1 = new Survey(email,age,gender,occupation,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10);
+            surveyList.add(s1);
+            s1.getSurvey(s1);
+            
+            
+            
+            success.show();
+ 
+        } catch (Exception e) {
+            unfilled.show();
+            System.out.println("try block didnt execute");
+        }
+        
     }
 
     @FXML
