005ec6b62fd794b97a085786711cbda97c460825
Sumaiya Tabassum <158046879+Srashid77@users.noreply.github.com>
2024-04-09 20:45:38 +0600

    add class

---
+++ Oop_Bangladesh_Red_Crescent_Society/OOP_Bangladesh_Red_Crescent_Society/src/Sumaiya/Trainer/HeaTestController.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Sumaiya.Trainer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Muntasir
+ */
+public class HeaTestController implements Initializable {
+
+    @FXML
+    private TextField oneNoAnswerTextField;
+    @FXML
+    private TextField twoNoAnswerTextField;
+    @FXML
+    private TextField threeNoAnswerTextField;
+    @FXML
+    private TextField fourNoAnswerTextField;
+    @FXML
+    private TextField fiveNoAnswerTextField;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void returnHomeButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void submitButtonOnClick(ActionEvent event) {
+    }
+    
+}
